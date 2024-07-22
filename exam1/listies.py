552dc686509d3166696466ccf32663374ee88426
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-22 10:51:16 -0400

    listies

--- exam1/listies.py
+++ exam1/listies.py
@@ -3,12 +3,14 @@ def drop_last(lst):
     """
     This function takes a list and returns a list with the last item removed.
     """
+    return lst[:-1]
     pass  # implement me
 
 def drop_mangle(lst):
     """
     This function takes a list and returns a list with the first two items AND last item removed.
     """
+    return lst[2:-1]
     pass  # implement me
 
 def add_item_front(lst, a):
@@ -16,6 +18,7 @@ def add_item_front(lst, a):
     This function takes a list and an item,
     returning the list with the item prepended to the list
     """
+    return [a]+lst
     pass  # implement me
 
 def add_item_end(lst, a):
@@ -23,17 +26,20 @@ def add_item_end(lst, a):
     This function takes a list and an item,
     returning the list with the item appended to the list
     """
+    return lst+[a]
     pass  # implement me
 
 def drop_first_two(lst):
     """
     This function takes a list and returns a list with the first two items removed.
     """
+    return lst[2:]
     pass  # implement me
 
 def drop_last_two(lst):
     """
     This function takes a list and returns a list with the last two items removed.
     """
+    return lst[0:-2]
     pass  # implement me
 
