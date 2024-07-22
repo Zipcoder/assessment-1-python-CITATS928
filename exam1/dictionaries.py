c17e03e80c93cf7291e1efc84b0a0417c7aea19d
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-22 11:28:59 -0400

    dictionary

--- exam1/dictionaries.py
+++ exam1/dictionaries.py
@@ -3,6 +3,11 @@ def delete_keys_from_dict(datadict, keylist):
     """
     Delete a list of keys from a dictionary
     """
+    for key in keylist:
+        if key in datadict:
+            del datadict[key]
+
+    return datadict
     pass
 
 def check_dict_for_key(datadict, key):
@@ -10,22 +15,36 @@ def check_dict_for_key(datadict, key):
     Check if a value exists in a dictionary
     (NO FOR loops!)
     """
+    if key in datadict.values():
+        return True
+    else:
+        return False
     pass
 
 def get_key_of_min_value(ddd):
     """
     Get the key of the minimum value from a dictionary
     """
+    return min(ddd,key=ddd.get)
     pass
 
 def get_key_of_max_value(ddd):
     """
     Get the key of the maximum value from a dictionary
     """
+    return max(ddd,key=ddd.get)
     pass
 
 def letterfreq(word):
     '''
     # Write a function that returns a dictionary of letter frequencies from a word
     '''
+    result={}
+    for cha in word:
+        if cha in result:
+            result[cha]+=1
+        else:
+            result[cha]=1
+
+    return result
     pass
